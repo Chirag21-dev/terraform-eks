3a122612959aa57628d0b77552c7ec585897316e
Chirag21-dev <chiragks21@outlook.com>
2025-08-22 10:03:02 +0530

    Update main.tf

--- eks_mod/main.tf
+++ eks_mod/main.tf
@@ -31,10 +31,10 @@ module "vpc" {
 module "eks" {
   source = "terraform-aws-modules/eks/aws"
 
-  cluster_name    = "my-eks-cluster"
-  cluster_version = "1.24"
+  name                   = "my-eks-cluster"
+  kubernetes_version     = "1.24"
 
-  cluster_endpoint_public_access = true
+  endpoint_public_access  = true
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
