b1ce6fd5392685a2a194d8e7f128d645f3510429
Chirag21-dev <chiragks21@outlook.com>
2025-08-29 10:40:42 +0530

    Update main.tf with removed eks addon version.

--- EKS/main.tf
+++ EKS/main.tf
@@ -103,7 +103,6 @@ resource "aws_eks_cluster" "devopsshack" {
 resource "aws_eks_addon" "ebs_csi_driver" {
   cluster_name    = aws_eks_cluster.devopsshack.name
   addon_name      = "aws-ebs-csi-driver"
-  addon_version = "v1.28.0-eksbuild.1"
   
   resolve_conflicts_on_create = "OVERWRITE"
   resolve_conflicts_on_update = "OVERWRITE"
