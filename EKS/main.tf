651c2d340f15bf18aa8c83f2f7b55c2ea4840b8f
Chirag21-dev <chiragks21@outlook.com>
2026-01-14 16:20:49 +0530

    Update main.tf

--- EKS/main.tf
+++ EKS/main.tf
@@ -175,6 +175,11 @@ resource "aws_iam_role_policy_attachment" "devopsshack_cluster_role_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
+resource "aws_iam_role_policy_attachment" "ebs_csi_driver_attach" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEBSCSIDriverPolicy"
+  role       = aws_iam_role.devopsshack_cluster_role.name
+}
+
 resource "aws_iam_role" "devopsshack_node_group_role" {
   name = "devopsshack-node-group-role-${random_id.suffix.hex}"
 
