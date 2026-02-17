88672cdc1ae6b007315fa4d01441e842a11a5e6f
Chirag21-dev <chiragks21@outlook.com>
2026-02-17 15:21:39 +0530

    Refactor Kubernetes provider configuration in Main.tf

--- EKS_LB/Main.tf
+++ EKS_LB/Main.tf
@@ -2,17 +2,13 @@ provider "aws" {
   region = "us-east-2"
 }
 
-provider "kubernetes" {
-  alias                  = "eks"
-  host                   = data.aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks.token
-}
-
-//provider "helm" {
-  provider "helm" {
+provider "helm" {
   kubernetes {
-    config_path = "~/.kube/config"
+    host                   = data.aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(
+      data.aws_eks_cluster.eks.certificate_authority[0].data
+    )
+    token = data.aws_eks_cluster_auth.eks.token
   }
 }
 
