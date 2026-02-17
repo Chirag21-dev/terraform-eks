f2c0f35251b68b7344868b66d5ddc2e6136a95ec
Chirag21-dev <chiragks21@outlook.com>
2026-02-17 15:02:17 +0530

    Add Kubernetes provider configuration to Main.tf

--- EKS_LB/Main.tf
+++ EKS_LB/Main.tf
@@ -1,6 +1,13 @@
 provider "aws" {
   region = "us-east-2"
 }
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+}
+
 provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.eks.endpoint
