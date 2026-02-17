e5ff2ec81c32a1a511dccf26086b1b6cb4e20f47
Chirag21-dev <chiragks21@outlook.com>
2026-02-17 19:53:51 +0530

    Refactor subnet and route table configurations

--- EKS_LB/Main.tf
+++ EKS_LB/Main.tf
@@ -16,12 +16,12 @@ resource "aws_vpc" "devopsshack_vpc" {
 }
 
 resource "aws_subnet" "devopsshack_subnet" {
-  
+  count=2
   vpc_id                  = aws_vpc.devopsshack_vpc.id
   
-  cidr_block              = cidrsubnet(aws_vpc.devopsshack_vpc.cidr_block, 8,0)
+  cidr_block              = cidrsubnet(aws_vpc.devopsshack_vpc.cidr_block, 8,count.index)
   
-  availability_zone= "us-east-1a"
+  availability_zone= element(["us-east-1a", "us-east-1b"], count.index)
   map_public_ip_on_launch = true
 
   tags = {
@@ -52,8 +52,8 @@ resource "aws_route_table" "devopsshack_route_table" {
 }
 
 resource "aws_route_table_association" "devopsshack_association" {
-  
-  subnet_id      = aws_subnet.devopsshack_subnet.id
+  count          = length(aws_subnet.devopsshack_subnet)
+  subnet_id      = aws_subnet.devopsshack_subnet[count.index].id
   
   route_table_id = aws_route_table.devopsshack_route_table.id
 }
@@ -100,7 +100,7 @@ resource "aws_eks_cluster" "devopsshack" {
   role_arn = aws_iam_role.devopsshack_cluster_role.arn
 
   vpc_config {
-    subnet_ids         = [aws_subnet.devopsshack_subnet.id]
+    subnet_ids         = aws_subnet.devopsshack_subnet[*].id
     security_group_ids = [aws_security_group.devopsshack_cluster_sg.id]
   }
 }
@@ -137,7 +137,7 @@ resource "aws_eks_node_group" "devopsshack" {
   cluster_name    = aws_eks_cluster.devopsshack.name
   node_group_name = "devopsshack-node-group"
   node_role_arn   = aws_iam_role.devopsshack_node_group_role.arn
-  subnet_ids      = [aws_subnet.devopsshack_subnet.id]
+  subnet_ids      = aws_subnet.devopsshack_subnet[*].id
   scaling_config {
     desired_size = 1
     max_size     = 1
