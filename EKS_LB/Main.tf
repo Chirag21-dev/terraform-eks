8cc665f2e4a1bb7c99c085cec9d8b106939f64c3
Chirag21-dev <chiragks21@outlook.com>
2026-02-17 16:45:52 +0530

    Fix subnet_id and subnet_ids to use list syntax

--- EKS_LB/Main.tf
+++ EKS_LB/Main.tf
@@ -52,8 +52,8 @@ resource "aws_route_table" "devopsshack_route_table" {
 }
 
 resource "aws_route_table_association" "devopsshack_association" {
-  count          = 2
-  subnet_id      = aws_subnet.devopsshack_subnet[count.index].id
+  
+  subnet_id      = aws_subnet.devopsshack_subnet.id
   
   route_table_id = aws_route_table.devopsshack_route_table.id
 }
@@ -100,7 +100,7 @@ resource "aws_eks_cluster" "devopsshack" {
   role_arn = aws_iam_role.devopsshack_cluster_role.arn
 
   vpc_config {
-    subnet_ids         = aws_subnet.devopsshack_subnet.id
+    subnet_ids         = [aws_subnet.devopsshack_subnet.id]
     security_group_ids = [aws_security_group.devopsshack_cluster_sg.id]
   }
 }
@@ -137,7 +137,7 @@ resource "aws_eks_node_group" "devopsshack" {
   cluster_name    = aws_eks_cluster.devopsshack.name
   node_group_name = "devopsshack-node-group"
   node_role_arn   = aws_iam_role.devopsshack_node_group_role.arn
-  subnet_ids      = aws_subnet.devopsshack_subnet.id
+  subnet_ids      = [aws_subnet.devopsshack_subnet.id]
   scaling_config {
     desired_size = 1
     max_size     = 1
